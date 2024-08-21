cf8fa73163a29cd5454e6c6c287f0396ccb40074
FirdausIqbal <firdausiqbal10@gmail.com>
2024-08-21 19:04:42 +0700

    update session settings

--- index.js
+++ index.js
@@ -29,6 +29,7 @@ app.use(session(
         saveUninitialized: true,
         cookie: {
             secure: process.env.ENV === "prod" ? true : false,
+            sameSite: false,
             maxAge: 10 * 60 * 1000
         }
     }
