4ab2ed47bde7af25943720ba5ae3f7146625f7b6
FirdausIqbal <firdausiqbal10@gmail.com>
2024-09-21 15:32:46 +0700

    less

--- config/database.js
+++ config/database.js
@@ -1,6 +1,7 @@
 import { Sequelize } from "sequelize";
+import pg from "pg"
 import env from "dotenv"
 env.config()
-const sequelize = new Sequelize(process.env.DB_URL, {dialect: "postgres"})
+const sequelize = new Sequelize(process.env.DB_URL, {dialect: "postgres", dialectModule: pg})
 
 export default sequelize;
